ad927ce596de8624fadcd19ec8b4ec331cec1f8b
Bailey Long <bailey-long@live.nmit.ac.nz>
2023-04-03 14:01:50 +1200

    Pigs on speed update

---
+++ TestGame/TestGame/Creature Classes/Pig.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGame.Creature_Classes
+{
+    public class Pig
+    {
+        // Fields
+        private int age;
+        private int weight;
+        private bool isPregnant;
+        private int gestationPeriod;
+        private int litterSize;
+
+        // Constructor
+        public Pig(int age, int weight, bool isPregnant, int gestationPeriod, int litterSize)
+        {
+            this.age = age;
+            this.weight = weight;
+            this.isPregnant = isPregnant;
+            this.gestationPeriod = gestationPeriod;
+            this.litterSize = litterSize;
+        }
+
+        // Properties
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public bool IsPregnant
+        {
+            get { return isPregnant; }
+            set { isPregnant = value; }
+        }
+
+        public int GestationPeriod
+        {
+            get { return gestationPeriod; }
+            set { gestationPeriod = value; }
+        }
+
+        public int LitterSize
+        {
+            get { return litterSize; }
+            set { litterSize = value; }
+        }
+
+        // Methods
+        public void Squeal()
+        {
+            Debug.WriteLine("The pig lets out a loud squeal!");
+        }
+
+        public void Root()
+        {
+            Debug.WriteLine("The pig roots around in the dirt looking for food.");
+        }
+
+        public void GiveBirth()
+        {
+            if (isPregnant)
+            {
+                Debug.WriteLine("The pig gives birth to a litter of " + litterSize + " piglets!");
+                isPregnant = false;
+            }
+            else
+            {
+                Debug.WriteLine("The pig is not pregnant.");
+            }
+        }
+    }
+
+}
--- TestGame/TestGame/Game1.cs
+++ TestGame/TestGame/Game1.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using TestGame.Creature_Classes;
 
 namespace TestGame
 {
     public class Game1 : Game
     {
-        Texture2D orcish_idol; // define 2dtexture for orcish idol
+        //setup pigs
+        List<Pig> pigs = new List<Pig>
+            {
+                new Pig(8, 90, false, 0, 0),
+                new Pig(10, 100, false, 0, 0),
+                new Pig(13, 300, true, 3, 5)
+            };
+        //get random to allow for rng later
+        Random rnd = new Random();
+
+        Texture2D piggy; // define 2dtexture for orcish idol
         Vector2 idolPosition; // postion of player
-        float idolSpeed; // speed of movement
+        float piggySpeed; // speed of movement
         SpriteEffects flip = SpriteEffects.FlipHorizontally; // to flip the model if it moves left or right
 
         private GraphicsDeviceManager _graphics;
@@ -24,9 +37,8 @@ namespace TestGame
         protected override void Initialize() // ran at start of game
         {
             // TODO: Add your initialization logic here
-            idolPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
-            _graphics.PreferredBackBufferHeight /2); // Sets position based on aspect ratio of screen
-            idolSpeed = 100f;
+            //idolPosition = new Vector2(rnd.Next(0, 800), rnd.Next(0, 800)); // Sets position based on aspect ratio of screen
+            piggySpeed = 100f;
 
             base.Initialize();
         }
@@ -36,7 +48,7 @@ namespace TestGame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            orcish_idol = Content.Load<Texture2D>("Sprites/piggy");
+            piggy = Content.Load<Texture2D>("Sprites/piggy");
         }
 
         protected override void Update(GameTime gameTime) // runs every frame
@@ -49,38 +61,43 @@ namespace TestGame
             //keyboard logic
             if (kstate.IsKeyDown(Keys.Up))
             {
-                idolPosition.Y -= idolSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                idolPosition.Y -= piggySpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             }
             if (kstate.IsKeyDown(Keys.Down))
             {
-                idolPosition.Y += idolSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                idolPosition.Y += piggySpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             }
             if (kstate.IsKeyDown(Keys.Left))
             {
-                idolPosition.X -= idolSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                idolPosition.X -= piggySpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
                 flip = SpriteEffects.FlipHorizontally;
             }
             if (kstate.IsKeyDown(Keys.Right))
             {
-                idolPosition.X += idolSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                idolPosition.X += piggySpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
                 flip = SpriteEffects.None;
             }
+            // Press space to make the pig squeal
+            if (kstate.IsKeyDown(Keys.Space))
+            {
+                //p1.Squeal();
+            }
             // set boundries to the icon so that it can not leave the game window
-            if (idolPosition.X > _graphics.PreferredBackBufferWidth - orcish_idol.Width / 2)
+            if (idolPosition.X > _graphics.PreferredBackBufferWidth - piggy.Width / 2)
             {
-                idolPosition.X = _graphics.PreferredBackBufferWidth - orcish_idol.Width / 2;
-            } else if (idolPosition.X < orcish_idol.Width / 2)
+                idolPosition.X = _graphics.PreferredBackBufferWidth - piggy.Width / 2;
+            } else if (idolPosition.X < piggy.Width / 2)
             {
-                idolPosition.X = orcish_idol.Width / 2;
+                idolPosition.X = piggy.Width / 2;
             }
             //again for the Y axis
-            if (idolPosition.Y > _graphics.PreferredBackBufferHeight - orcish_idol.Height / 2)
+            if (idolPosition.Y > _graphics.PreferredBackBufferHeight - piggy.Height / 2)
             {
-                idolPosition.Y = _graphics.PreferredBackBufferHeight - orcish_idol.Height / 2;
+                idolPosition.Y = _graphics.PreferredBackBufferHeight - piggy.Height / 2;
             }
-            else if (idolPosition.Y < orcish_idol.Height / 2)
+            else if (idolPosition.Y < piggy.Height / 2)
             {
-                idolPosition.Y = orcish_idol.Height / 2;
+                idolPosition.Y = piggy.Height / 2;
             }
 
             base.Update(gameTime);
@@ -92,17 +109,19 @@ namespace TestGame
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw( // draw the sprite with added parameters
-                orcish_idol, // sprite reference
-                idolPosition, // co-ordinates to draw
-                null, // Rectangle ?
-                Color.White, // color for sprite, white to leave as is
-                0f, // rotation
-                new Vector2 (orcish_idol.Width / 2, orcish_idol.Height / 2), // define sprite origin as center of image
-                1.5f, // image scale
-                flip, // any sprite effects
-                0f // sprite depth
-                );
+            foreach (Pig Pig in pigs) {
+                _spriteBatch.Draw( // draw the sprite with added parameters
+                    piggy, // sprite reference
+                    new Vector2(rnd.Next(0, 800), rnd.Next(0, 800)), // co-ordinates to draw
+                    null, // Rectangle ?
+                    Color.White, // color for sprite, white to leave as is
+                    0f, // rotation
+                    new Vector2(piggy.Width / 2, piggy.Height / 2), // define sprite origin as center of image
+                    1.5f, // image scale
+                    flip, // any sprite effects
+                    0f // sprite depth
+                    );
+            }
             _spriteBatch.End();
 
             base.Draw(gameTime);
