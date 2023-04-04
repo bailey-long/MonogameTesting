cd469a4f41273af1cdee52026a6d0e02647ed510
Loopypew <fivebailey2@gmail.com>
2023-04-05 01:47:03 +1200

    Added code to detect if hovering over pig

--- TestGame/TestGame/Game1.cs
+++ TestGame/TestGame/Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
 using TestGame.Creature_Classes;
@@ -11,7 +12,15 @@ namespace TestGame
 {
     public class Game1 : Game
     {
-        //setup pigs
+        // get random to allow for rng later
+        Random rnd = new Random();
+        // referece for SpriteEffects
+        SpriteEffects flip;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // setup pigs
         List<Pig> pigs = new List<Pig>
             {
                 new Pig(8, "Piggy", 90, false, 0, 0),
@@ -21,22 +30,12 @@ namespace TestGame
                 new Pig(13, "Napoleon", 300, true, 3, 5),
                 new Pig(13, "Skotizo", 300, true, 3, 5),
                 new Pig(13, "Vorky", 300, true, 3, 5),
-                new Pig(15, "Poglin", 100, false, 0, 0)
+                new Pig(15, "Poglin", 100, false, 0, 0),
             };
-        // Create a dictionary to hold the spawn points of the pigs.
+        // Create dictionaries to hold the spawn points and orientation of the pigs.
         Dictionary<Pig, Vector2> pigSpawnPoints = new Dictionary<Pig, Vector2>();
-        // Create a dictionary to hold the spawn orientation of the pigs
         Dictionary<Pig, SpriteEffects> pigOrientation = new Dictionary<Pig, SpriteEffects>();
-
-        //get random to allow for rng later
-        Random rnd = new Random();
-
-        Texture2D piggy; // define 2dtexture for orcish idol
-        float piggySpeed; // speed of movement
-        SpriteEffects flip;
-
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        Texture2D piggy; // define 2dtexture for the pig
 
         public Game1()
         {
@@ -50,13 +49,10 @@ namespace TestGame
         protected override void Initialize() // ran at start of game
         {
             // TODO: Add your initialization logic here
-            //idolPosition = new Vector2(rnd.Next(0, 800), rnd.Next(0, 800)); // Sets position based on aspect ratio of screen
-            piggySpeed = 100f;
             foreach (Pig pig in pigs)
             {
-                // Define the spawn points for each pig and add it to the dictionary to be called 
-                // in the draw function.
-                Vector2 spawnPoint = new Vector2(rnd.Next(0, 1200), rnd.Next(0, 680));
+                // Define the spawn points for each pig and add it to the dictionary to be called in the draw function.
+                Vector2 spawnPoint = new Vector2(rnd.Next(0, 1100), rnd.Next(0, 640));
                 // Randomly choose the direction of each pig
                 int direction = rnd.Next(0, 2);
                 // add pig direction to dictionary
@@ -72,7 +68,6 @@ namespace TestGame
                 // add pig spawnpoint to dictionary
                 pigSpawnPoints.Add(pig, spawnPoint);
             }
-
             base.Initialize();
         }
 
@@ -90,6 +85,12 @@ namespace TestGame
                 Exit();
             var mouseState = Mouse.GetState();
             var mousePoint = new Point(mouseState.X, mouseState.Y);
+            var rectangle = new Rectangle(mousePoint.X, mousePoint.Y, this.piggy.Width, this.piggy.Height);
+
+            if (rectangle.Contains(mousePoint))
+            {
+                Debug.WriteLine(this.piggy.Width);
+            }
             
 
             //Hover over pig with mouse cursor
@@ -114,23 +115,26 @@ namespace TestGame
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            // Instantiate each pig from the list
-            foreach (Pig Pig in pigs) {
-                Vector2 pigSpawnPoint;
-                if (pigSpawnPoints.TryGetValue(Pig, out pigSpawnPoint) && pigOrientation.TryGetValue(Pig, out flip))  {
-                    _spriteBatch.Draw( // draw the sprite with added parameters
-                        piggy, // sprite reference
-                        pigSpawnPoint, // co-ordinates to draw
-                        null, // Rectangle ?
-                        Color.White, // color for sprite, white to leave as is
-                        0f, // rotation
-                        new Vector2(piggy.Width / 2, piggy.Height / 2), // define sprite origin as center of image
-                        1.5f, // image scale
-                        flip, // any sprite effects
-                        0f // sprite depth
-                        );
+            // draw pigs first frame.
+                // Instantiate each pig from the list
+                foreach (Pig Pig in pigs)
+                {
+                    Vector2 pigSpawnPoint;
+                    if (pigSpawnPoints.TryGetValue(Pig, out pigSpawnPoint) && pigOrientation.TryGetValue(Pig, out flip))
+                    {
+                        _spriteBatch.Draw( // draw the sprite with added parameters
+                            piggy, // sprite reference
+                            pigSpawnPoint, // co-ordinates to draw
+                            null, // Rectangle ?
+                            Color.White, // color for sprite, white to leave as is
+                            0f, // rotation
+                            new Vector2(piggy.Width / 2, piggy.Height / 2), // define sprite origin as center of image
+                            1.5f, // image scale
+                            flip, // any sprite effects
+                            0f // sprite depth
+                            );
+                    }
                 }
-            }
             _spriteBatch.End();
 
             base.Draw(gameTime);
